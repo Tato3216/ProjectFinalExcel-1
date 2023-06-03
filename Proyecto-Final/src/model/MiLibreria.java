8c019216141793b438e2c0374396f1d823b9cbbb
Joab Ramirez <kennet.guzman.ramirez@gmail.com>
2023-06-03 02:54:19 -0600

    Se sube la clase Libreria

---
+++ Proyecto-Final/src/model/MiLibreria.java
@@ -0,0 +1,15 @@
+package model;
+
+public class MiLibreria {
+
+    //Metodo Suma
+     public static double suma(double a, double b) {
+        return a + b;
+    }
+
+     //Metodo Multiplicacion
+    public static double multiplicacion(double a, double b) {
+        return a * b;
+    }
+
+}
